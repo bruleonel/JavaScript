e2af556e5cb5fb329335ecc7334bc41f8bbee1bb
Bruna Leonel <psbru2012@gmail.com>
2022-12-27 19:53:11 -0300

    Exercícios/aula-arrays

---
+++ aulaArray/foreach2.js
@@ -0,0 +1,9 @@
+
+const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+console.log(aprovados)
+console.log('')
+
+aprovados.forEach(function(nome, indice) {
+    console.log(`${indice + 1}) ${nome}`)
+})
+console.log('')
---
+++ exercicios02/exercicio03.js
@@ -0,0 +1,9 @@
+/* Desenvolva uma função que receba o salário/hora
+mais a quantidade de horas trabalhas no mês e calcule o
+salário total */
+
+function calcularSalario(salario, horas) {
+    return salario * horas
+}
+
+console.log(calcularSalario(40, 150))
