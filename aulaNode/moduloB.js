58c494ef3b38e35714f8d895b5677a0ead4e42a9
Bruna Leonel <psbru2012@gmail.com>
2022-12-29 20:23:15 -0300

    estudando sobre importação/exportação

---
+++ aulaNode/moduloB.js
@@ -0,0 +1,8 @@
+//Melhor forma de exportação:
+
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite() {
+        return 'Boa noite!'
+    }
+}
---
+++ aulaNode/moduloCliente.js
@@ -0,0 +1,13 @@
+// Importação
+
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloB.bomDia)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
